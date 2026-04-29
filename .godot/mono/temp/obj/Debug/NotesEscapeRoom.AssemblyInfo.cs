f80b23408b3cdbb24a04835d9879dca8e1d3eab8
Andrew Kehler <andrew.kehler7@gmail.com>
2026-04-28 19:10:49 -0700

    Added all books into bookshelf with working interactions.

--- .godot/mono/temp/obj/Debug/NotesEscapeRoom.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/NotesEscapeRoom.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotesEscapeRoom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ca88ad3123b1356d0102db7d2e10c7b72085f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5667077a51827ea8bd71b919c43a33033eaad8c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotesEscapeRoom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotesEscapeRoom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
